6f8652aa1cb5fe2ddfaa59e78f3f7727a2a6bebd
dfouch-csumb <dfouch@csumb.edu>
2024-07-18 16:24:55 -0700

    commented Ability.java

--- src/main/java/Ability.java
+++ src/main/java/Ability.java
@@ -1,2 +1,12 @@
+/**
+ * This is a plain old java project (POJO) that represents an interface for various attack ability classes,
+ * which is used for a turn-based text game of a
+ * This class is extended by Attack.java, and is implemented by MeleeAttack.java & RangedAttack.java.
+ * Link to GitHub Repo: <a href="https://github.com/CST-338/strategy-pattern-dfouch-csumb">...</a>
+ * @author Dakota L. Fouch
+ * @since 07/18/2024
+ * CST 338 Software Design with Dr. C
+ * HW3: Strategy Pattern (Ability.java)
+ */
 public interface Ability {
 }
