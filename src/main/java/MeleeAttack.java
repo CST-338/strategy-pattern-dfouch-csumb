7456e3ab174b446f4e0783bfda744502dca8a749
dfouch-csumb <dfouch@csumb.edu>
2024-07-18 16:38:27 -0700

    added header comment to MeleeAttack.java

--- src/main/java/MeleeAttack.java
+++ src/main/java/MeleeAttack.java
@@ -1,3 +1,13 @@
+/**
+ * This is a plain old java project (POJO) that represents a melee attack ability based on an attack ability interface.
+ * which is used for a turn-based text game of a
+ * This class implements interfaces, Attack.java (which extends Ability.java), and is a sibling class RangedAttack.java.
+ * Link to GitHub Repo: <a href="https://github.com/CST-338/strategy-pattern-dfouch-csumb">...</a>
+ * @author Dakota L. Fouch
+ * @since 07/18/2024
+ * CST 338 Software Design with Dr. C
+ * HW3: Strategy Pattern (MeleeAttack.java)
+ */
 public class MeleeAttack implements Attack {
 
     Monster attacker;
